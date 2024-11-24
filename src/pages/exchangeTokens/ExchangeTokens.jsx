4afde7ecdea69c49208d6b1442219a43ba2296c6
Ronald Pardo <koren.yin@outlook.es>
2024-11-23 23:09:10 -0500

    rebasing

--- src/pages/exchangeTokens/ExchangeTokens.jsx
+++ src/pages/exchangeTokens/ExchangeTokens.jsx
@@ -6,7 +6,7 @@ import "./ExchangeTokens.css"
 function ExchangeTokens(){
 
     const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
-    const contractAddress = "0x42699A7612A82f1d9C36148af9C77354759b210b"; 
+    const contractAddress = "0xe135783649BfA7c9c4c6F8E528C7f56166efC8a6"; 
     const privateKey = "8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63";
 
     const [checkAccount, setCheckAccount] = useState("");
